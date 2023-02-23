88a28545c635cc48b7cbc21e5c4235d7fe53a4a4
khalfanathman <khalfanathman12@gmail.com>
2023-02-22 19:57:48 -0800

    adding 0main and main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,13 @@
 
 #include <stdio.h>
 
+/**
+ * positive_or_negative - Determine if a number is positive, negative or zero
+ *
+ * @i: The integer to be checked
+ *
+ * Return: Nothing
+ */
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
