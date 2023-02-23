74844bab7f4d76eb31d8ff9a2de75fe0eebab085
khalfanathman <khalfanathman12@gmail.com>
2023-02-22 23:01:40 -0800

    adding last proj

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,6 +4,21 @@
 #include <stdio.h>
 
 /**
+ * struct date_s - Struct for date
+ * @day: integer
+ * @month: integer
+ * @year: integer
+ *
+ * Description: Struct for date
+ */
+typedef struct date_s
+{
+	int day;
+	int month;
+	int year;
+} date_t;
+
+/**
  * positive_or_negative - Determine if a number is positive, negative or zero
  *
  * @i: The integer to be checked
@@ -21,4 +36,22 @@ void positive_or_negative(int i);
  * Return: largest number
  */
 int largest_number(int a, int b, int c);
+/**
+ * print_remaining_days - print how many days are left in the year
+ *
+ * @month: current month (integer)
+ * @day: current day (integer)
+ * @year: current year (integer)
+ *
+ * Return: Nothing
+ */
+void print_remaining_days(int month, int day, int year);
+/**
+ * convert_day - convert day of month to day of year
+ *
+ * @date: date to calculate day of year for
+ *
+ * Return: day of year
+ */
+int convert_day(int month,int day);
 #endif /* MAIN_H */
