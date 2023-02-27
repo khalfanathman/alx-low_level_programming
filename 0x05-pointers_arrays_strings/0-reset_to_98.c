6d00e546fb20c9c065e6b620020120c4e90c0333
khalfanathman <khalfanathman12@gmail.com>
2023-02-27 02:05:39 -0800

    edit for betty

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 /**
- * reset_to_98 - check the code 
- *
+ * reset_to_98 - check the code
+ * @n: pointer which is reassigned
  * Return: Always 0.
  */
 void reset_to_98(int *n)
 {
 	*n = 98;
-	return ;
+
 }
