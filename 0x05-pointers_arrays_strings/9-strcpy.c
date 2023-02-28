01a2d5343d42477d086cc8506d6f3956266021b1
khalfanathman <khalfanathman12@gmail.com>
2023-02-27 17:14:06 -0800

    strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <string.h>
+/**
+ * _strcpy - main function to copy
+ *
+ * @dest: destination to copy
+ * @src: src
+ *
+ * Return: a character value
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= strlen(src); i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
