f84e6235fc83c6224cd4e408393840f1ac8a17f4
khalfanathman <khalfanathman12@gmail.com>
2023-03-06 00:37:08 -0800

    add main.h

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H_
+#define MAIN_H_
+
+/* Prototype for _memset function
+ * @s: pointer to memory area
+ * @b: constant byte used to set memory area
+ * @n: number of bytes to set
+ *
+ * Return: pointer to memory area
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H_ */
+
