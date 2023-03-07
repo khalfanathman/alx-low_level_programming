7cc50e27584f090e8c5ed2f6801ce6bfc557ae32
khalfanathman <khalfanathman12@gmail.com>
2023-03-06 18:09:15 -0800

    finishing

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,63 +2,74 @@
 #define MAIN_H_
 
 /**
- * _memset - function that returns a pointer
- *  to the memory area s
- * @s: pointer to memory area
- * @b: constant byte used to set memory area
+ * _memset - fill memory with a constant byte
+ * @s: pointer to memory area to fill
+ * @b: value to set
  * @n: number of bytes to set
  *
- * Return: pointer to memory area
+ * Return: pointer to memory area s
  */
 char *_memset(char *s, char b, unsigned int n);
+
 /**
- * _memcpy - function that copies memory area
- * @dest: pointer to the destination memory area
- * @src: pointer to the source memory area
- * @n: number of bytes to be copied
+ * _memcpy - copy memory area
+ * @dest: pointer to destination memory area
+ * @src: pointer to source memory area
+ * @n: number of bytes to copy
  *
- * Return: a pointer to the destination memory area
+ * Return: pointer to destination memory area
  */
 char *_memcpy(char *dest, char *src, unsigned int n);
+
 /**
- * _strchr - function that locates a character in a string
- * @s: pointer to the string
- * @c: character to be located
+ * _strchr - locate character in string
+ * @s: pointer to string
+ * @c: character to locate
  *
- * Return: a pointer to the
- *  first occurrence of the character c in
- *  the string s,
- * or NULL if the character is not found
+ * Return: pointer to first occurrence of character c in string s or NULL if not found
  */
 char *_strchr(char *s, char c);
 
 /**
- * _strspn - function that calculates the length of a prefix substring
- * @s: pointer to the string
- * @accept: pointer to the string containing the characters to match
+ * _strspn - get length of a prefix substring
+ * @s: pointer to string
+ * @accept: pointer to string containing characters to match
  *
- * Return: the number of bytes in
- *  the initial segment of s which consist only of
- *  bytes from accept
+ * Return: number of bytes in the initial segment of s consisting of only bytes from accept
  */
 unsigned int _strspn(char *s, char *accept);
 
 /**
- * _strpbrk - function that searches a string for any of a set of bytes
- * @s: pointer to the string
- * @accept: pointer to the string containing the characters to search for
+ * _strstr - locate a substring
+ * @haystack: pointer to string
+ * @needle: pointer to substring
  *
- * Return: a pointer to the byte in s that matches one of the bytes in accept,
- * or NULL if no such byte is found
- */
-char *_strpbrk(char *s, char *accept);
-/**
- * _strstr - locates a substring.
- *
- * @haystack: string to search in.
- * @needle: substring to search for.
- *
- * Return: pointer to the beginning of the located substring or NULL if not found.
+ * Return: pointer to the beginning of the located substring or NULL if not found
  */
 char *_strstr(char *haystack, char *needle);
+
+/**
+ * _strpbrk - searches a string for any of a set of bytes
+ * @s: the string to search through
+ * @accept: the set of bytes to search for
+ *
+ * Return: a pointer to the byte in `s` that matches one of the bytes in `accept`
+ *         or NULL if no such byte is found
+ */
+char *_strpbrk(char *s, char *accept);
+
+/**
+ * print_chessboard - print the chessboard
+ * @a: pointer to array of 8 arrays of 8 characters each
+ */
+void print_chessboard(char (*a)[8]);
+
+/**
+ * print_diagsums - print the sums of the two diagonals of a square matrix of integers
+ * @a: pointer to the square matrix of integers
+ * @size: size of the matrix
+ */
+void print_diagsums(int *a, int size);
+
 #endif /* MAIN_H_ */
+
