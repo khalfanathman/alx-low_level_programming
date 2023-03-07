fbbacb494c091983720c5095b18bcde173c54c8c
khalfanathman <khalfanathman12@gmail.com>
2023-03-06 18:19:16 -0800

    shorten code

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,5 +1,4 @@
-#include <main.h>
-
+#include <stdio.h>
 /**
  * _strstr - locates a substring.
  *
@@ -8,46 +7,28 @@
  *
  * Return: ptr to the strt of the lctd substr or NULL if othws.
  */
+
 char *_strstr(char *haystack, char *needle)
 {
-	int i, l, count = 0, length = 0, stalength = 0;
-	char bool = 0;
+	int i = 0, j = 0, k = 0;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	while (haystack[i])
 	{
-		length++;
-	}
-	for (l = 0; haystack[l] != '\0'; l++)
-	{
-		stalength++;
-	}
-	count = length;
-	for (i = 0; i <= stalength; i++)
-	{
-		if (haystack[i] == needle[0])
+		j = i, k = 0;
+
+		while (needle[k] && haystack[j] == needle[k])
 		{
-			int j = i, k = 0;
-
-			while (count > 0)
-			{
-				if (haystack[j] == needle[k])
-				{
-					bool = 1;
-					count--;
-					j++;
-					k++;
-				}
-				else
-				{
-					bool = 1;
-					break;
-				}
-			}
-			if (bool && count == 0)
-			{
-				return (&haystack[i]);
-			}
+			j++;
+			k++;
 		}
+
+		if (!needle[k])
+		{
+			return (&haystack[i]);
+		}
+
+		i++;
 	}
+
 	return (NULL);
 }
