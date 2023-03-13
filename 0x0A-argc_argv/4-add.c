e014a2ccaaa7857840292fe3a95b7766c8705f17
khalfanathman <khalfanathman12@gmail.com>
2023-03-13 01:10:28 -0700

    4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,19 +15,23 @@ int main(int argc, char *argv[])
 	for (i = 1; i < argc; i++)
 	{
 		track = 1;
+
 		if (argv[i][0] == '-' && isdigit(argv[i][1]))
 		{
 			sum -= atoi(&argv[i][1]);
 			continue;
 		}
+
 		if (!isdigit(argv[i][0]))
 		{
 			printf("%s\n", "Error");
 			return (1);
 		}
+
 		sum += atoi(argv[i]);
 	}
-	track == 1 ? (printf("%d\n", sum),0):(printf("%d\n", sum),1);
+
+	track == 1 ? (printf("%d\n", sum), 0) : (printf("%d\n", sum), 1);
 	return (0);
 }
 
