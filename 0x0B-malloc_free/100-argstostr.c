4c6f587a7e314095ef5d35270df2321b918a3609
khalfanathman <khalfanathman12@gmail.com>
2023-03-14 17:23:02 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+
 /**
  * argstostr - Concatenates all the strings in an array into a single string,
  *             separated by newline characters.
@@ -11,22 +12,21 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int i, j, k, *length;
+	int i, j, k, len, *length;
 	char *fin_string, *p;
-	/* Allocate memory for an array to store the length of each string */
+
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	length = malloc(ac * sizeof(int));
 	if (length == NULL)
 		return (NULL);
-	/* Compute the length of each string and store it in the length array */
 	for (i = 0; i < ac; i++)
 	{
-		length[i] = 0;
-		for (j = 0; av[i][j] != '\0'; j++)
-			length[i]++;
+		len = 0;
+		while (av[i][len])
+			len++;
+		length[i] = len;
 	}
-	/* Allocate memory for the final string */
 	fin_string = malloc((ac + 1) * sizeof(char *));
 	if (fin_string == NULL)
 	{
@@ -34,20 +34,14 @@ char *argstostr(int ac, char **av)
 		return (NULL);
 	}
 	p = fin_string;
-	/*
-	 * Copy each string into the final string, separated by newline characters
-	 * and terminate the final string with a null character.
-	 */
 	for (i = 0; i < ac; i++)
 	{
-		for (k = 0; k <= length[i]; k++)
-		{
-			*p = (k == length[i]) ? '\n' : av[i][k];
-			p++;
-		}
+		for (k = 0; k < length[i]; k++)
+			*p++ = av[i][k];
+		*p++ = '\n';
 	}
 	*p = '\0';
-	/* Free the length array and return the final string */
 	free(length);
 	return (fin_string);
 }
+
