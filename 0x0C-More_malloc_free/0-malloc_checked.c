1ee505f7711e9023860099b82f052d7c158b6bf9
Khalfan Athman <khalfanathman12@gmail.com>
2023-03-17 05:47:43 +0300

    0-malloc_checked

--- 0x0C-More_malloc_free/0-malloc_checked.c
+++ 0x0C-More_malloc_free/0-malloc_checked.c
@@ -9,14 +9,18 @@
  *         if malloc fails, the malloc_checked function should cause normal
  *         process termination with a status value of 98
  */
-void *malloc_checked(unsigned int b) {
-  void *ptr;
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
 
-  ptr = malloc(b);
-  if (ptr == NULL) {
-    /* If malloc fails, exit with a status value of 98 */
-    exit(98);
-  }
-  /* Return pointer to the allocated memory */
-  return ptr;
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		/* If malloc fails, exit with a status value of 98 */
+		exit(98);
+	}
+
+	/* Return pointer to the allocated memory */
+	return ptr;
 }
