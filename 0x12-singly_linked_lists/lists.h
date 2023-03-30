2c8ba177ca6854dce3643ba165dcdb2d37e746c5
Khalfan Athman <khalfanathman12@gmail.com>
2023-03-30 16:46:07 +0300

    a function that prints all the elements of a list_t list.

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -15,7 +15,13 @@ typedef struct list_s
     unsigned int len;
     struct list_s *next;
 } list_t;
-
+/**
+ * print_list - Prints all the elements of a linked list.
+ *
+ * @h: Pointer to the head of the list_t list.
+ *
+ * Return: The number of nodes in the list.
+ */
 size_t print_list(const list_t *h);
 
 #endif  /* LISTS_H */
